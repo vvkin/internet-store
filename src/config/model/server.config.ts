38de872992f02673bbb80502ef2f8be1be97f5a4
vvkin <vkincur@gmail.com>
2021-11-06 15:23:02 +0200

    Set up basic project structure

---
+++ src/config/model/server.config.ts
@@ -0,0 +1,5 @@
+export interface ServerConfig {
+    host: string;
+    port: number;
+    logger: boolean;
+}
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,20 @@
-import { createServer } from 'http';
+import Fastify, { FastifyPluginAsync } from 'fastify';
+import { SERVER_CONFIG, config } from './config/config';
+import { app } from './app';
 
-const server = createServer((req, res) => {
-    res.end('<h1>Hello, world!</h1>');
-});
+const { port, host, logger } = config[SERVER_CONFIG];
 
-server.listen(3000);
+const bootstrap = async (app: FastifyPluginAsync) => {
+    try {
+        const server = Fastify({ logger });
+        server.register(app);
+        server.listen(port, host, () => {
+            console.log(`Server started on http://${host}:${port}`);
+        });
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
+    }
+};
+
+bootstrap(app);
